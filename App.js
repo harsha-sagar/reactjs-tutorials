55dad5d9d1a12419f6e9db8b1bc8e453702a4a9a
km-sagar <harsha@liftoffllc.com>
2016-08-17 19:01:36 +0530

    understanding with component's properties

--- App.js
+++ App.js
@@ -1,14 +1,55 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component{
+/*
+1st way of utilizing properties passed to the component
+*/
+    // render(){
+    //     return <h1>{this.props.txt}</h1>;
+    // }
+
+/*
+2nd way of utilizing properties passed to the component
+*/
     render(){
-        return (
-            <div>
-                <h1>Hello world</h1>
-                <b>bold</b>
-            </div>
-        );
+        let txt = this.props.txt;
+        return <h1>{txt}</h1>;
     }
+
 }
 
-export default App
+/*
+specifying constraints on the properties being passed to the component
+*/
+App.propTypes = {
+  txt: React.PropTypes.string,
+/*
+indicates txt property received, should be string
+*/
+
+  cat: React.PropTypes.number.isRequired
+/*
+indicates cat property received, should be number & should be passed to the component
+*/
+};
+
+App.defaultProps = {
+    txt: 'this is the default text'
+}
+
+/*
+overiding default property value
+*/
+// ReactDOM.render(
+//     <App cat={5} txt="this is the props text"/>,
+//     document.getElementById('app')
+// );
+
+/*
+working default property value
+*/
+ReactDOM.render(
+    <App cat={5}/>,
+    document.getElementById('app')
+);
