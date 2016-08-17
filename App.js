b5dd70684e619fc8c0b4993177b3a1f1e2eb4a62
km-sagar <harsha@liftoffllc.com>
2016-08-17 19:04:57 +0530

    necessary cleanup for render method

--- App.js
+++ App.js
@@ -32,20 +32,4 @@ Also works :-  return <div>
     }
 }
 
-/*
-approach 2 for creating a component
-this is class component, which maintains state
-*/
-// class App extends React.Component{
-//     render(){
-//         return React.createElement('h1',null,'hello guys');
-//     }
-// }
-
-/*
-approach 3 for creating a component
-this is functional component, which is stateless
-*/
-// const App = () => <h1>Hello eggheads</h1>
-
 export default App
