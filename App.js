d4594464be6b87301c98719e22bb32ddbe71188e
km-sagar <harsha@liftoffllc.com>
2016-08-17 18:28:56 +0530

    understanding working of render method

--- App.js
+++ App.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 class App extends React.Component{
     render(){
-        return <h1>Hello world</h1>
+        return (
+            <div>
+                <h1>Hello world</h1>
+                <b>bold</b>
+            </div>
+        );
+
+/*
+at any given time, only one node should be returned as shown
+Does not works :- return <h1>Hello world</h1><b>bold</b>
+*/
+
+/*
+Does not works :-  return
+                    <div>
+                        <h1>Hello world</h1>
+                        <b>bold</b>
+                    </div>
+*/
+
+/*
+Also works :-  return <div>
+                        <h1>Hello world</h1>
+                        <b>bold</b>
+                    </div>
+*/
+
     }
 }
 
