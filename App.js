c82f29e9f5871153a1c35546ab89281dccd687ea
km-sagar <harsha@liftoffllc.com>
2016-08-21 22:11:29 +0530

    cleanup before starting composable example

--- App.js
+++ App.js
@@ -1,70 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/*
-owner component or parent component or composite component
-*/
 class App extends React.Component{
     constructor(){
         super();
         this.state = {
-            red: 'red',
-            green: 'green',
-            blue: 'blue',
-            cat: 0
+            red: 0,
         };
         this.update = this.update.bind(this);
     }
     update(e){
-        console.log(ReactDOM.findDOMNode(this.refs.red).childNodes[0].value);
         this.setState({
             red: ReactDOM.findDOMNode(this.refs.red).childNodes[0].value,
-            green: ReactDOM.findDOMNode(this.refs.green).childNodes[0].value,
-            blue: ReactDOM.findDOMNode(this.refs.blue).childNodes[0].value
         });
     }
     render(){
         return (
             <div>
                 <Widget ref="red"
-                        txt={this.state.red}
-                        update={this.update}/>
-                <Widget ref="green"
-                        txt={this.state.green}
-                        update={this.update}/>
-                <Widget ref="blue"
-                        txt={this.state.blue}
+                        val={this.state.red}
                         update={this.update}/>
             </div>
         );
     }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-};
-
-App.defaultProps = {
-    txt: 'this is the default text'
-}
-
-/*
-child component or ownee component
-*/
 class Widget extends React.Component{
     render(){
         return (
             <div>
                 <input type="text"
-                       onChange={this.props.update} value={this.props.txt}/>
-                <h1>{this.props.txt}</h1>
+                       onChange={this.props.update} value={this.props.val}/>
+                <h1>{this.props.val}</h1>
             </div>
         );
     }
 }
 
 ReactDOM.render(
-    <App cat={5}/>,
+    <App />,
     document.getElementById('app')
 );
